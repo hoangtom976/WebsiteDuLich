ffbefcb9a9a59bb98055c7e3c5eabdd59cd22217
Khang <hoangtom976@gmail.com>
2026-03-21 00:19:20 +0700

    capnhat21/3/2

--- backend/src/main/java/com/dulich/backend/config/CauHinhCloudinary.java
+++ backend/src/main/java/com/dulich/backend/config/CauHinhCloudinary.java
@@ -13,7 +13,6 @@ public class CauHinhCloudinary {
         return new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", "ddpreqmwn",
                 "api_key", "936396337116263",
-                "api_secret", "KZ0K0vkNKPtv8sjBX-I3pQIXrz0"
-        ));
+                "api_secret", "KZ0K0vkNKPtv8sjBX-I3pQIXrz0"));
     }
 }
